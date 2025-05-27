5aca7c3e5079a9b3e2928d38b5009e8e13b4cfa3
Erik <erikricku@gmail.com>
2025-05-27 12:14:03 +0200

    Formattato CSS e sceltaLivello per validazione

--- php/sceltaLivello.php
+++ php/sceltaLivello.php
@@ -62,7 +62,6 @@ mysqli_close($connessione);
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selezione Livello - Platform Game</title>
     <?php require_once 'head.php';?>
